dd126770cf3fba1fe71e89fbe11f24593cef51b1
tashvitgp <gangradetashvi@gmail.com>
2026-02-05 00:14:24 +0530

    Importing model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,62 +1,63 @@
-import pandas as pd
 import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-from src.utils import save_obj
 
-# Step 1: Define where files will be stored using a Dataclass
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', "train.csv")
-    test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacts', "data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
-# Step 2: Define the logic for reading and splitting data
 class DataIngestion:
     def __init__(self):
-        # We use a standard __init__ here to link the config to this class
-        self.ingestion_config = DataIngestionConfig()
-    
+        self.ingestion_config=DataIngestionConfig()
+
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            # Reading the dataset from your local directory
-            df = pd.read_csv('notebook/data/stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            # Create the 'artifacts' folder if it doesn't exist
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            # Save the raw data before splitting
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            # Save the split datasets to the paths defined in the config
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            return (
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
             )
-            
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
         
-# Step 3: Execution block
-if __name__ == "__main__":
-    obj = DataIngestion()
-    # The method name here must match the one defined above
-    train_data, test_data = obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
 import sys
-import dill
-from src.exception import CustomException
 
+import numpy as np 
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -23,3 +25,29 @@ def load_object(file_path):
 
 # Backwards compatibility
 save_obj = save_object
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    report = {}
+    try:
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = list(models.keys())[i]
+
+            model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
