5f790995c09e5ec0ebe3379002672e8f3c35d915
tashvitgp <gangradetashvi@gmail.com>
2026-02-09 20:18:04 +0530

    Our last commit

--- src/utils.py
+++ src/utils.py
@@ -4,37 +4,40 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def load_object(file_path):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
-    except Exception as e:
-        raise CustomException(e, sys)
-
-# Backwards compatibility
-save_obj = save_object
-
-def evaluate_models(X_train,y_train,X_test,y_test,models):
-    report = {}
-    try:
+        report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            param = list(models.keys())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
@@ -50,4 +53,10 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
